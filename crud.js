f27b98b1760a258ec4b6f29222b759eeb8ff82a6
mjasper30 <jasper.macaraeg42@gmail.com>
2023-07-13 21:26:55 +0800

    adding of questions

---
+++ crud.js
@@ -0,0 +1,32 @@
+function addQuestion() {
+  $("#addQuestionForm").submit(function (e) {
+    e.preventDefault(); // Prevent the form from submitting normally
+
+    // Get the form data
+    var formData = {
+      yourQuestion: $("#yourQuestion").val(),
+      choiceOne: $("#choiceOne").val(),
+      choiceTwo: $("#choiceTwo").val(),
+      choiceThree: $("#choiceThree").val(),
+      choiceFour: $("#choiceFour").val(),
+      correctAnswer: $('input[name="correctAnswer"]:checked').val(),
+    };
+
+    // Send the AJAX request
+    $.ajax({
+      url: "add_question.php", // Replace with the actual URL to your server-side script
+      type: "POST",
+      data: formData,
+      success: function (response) {
+        // Handle the successful response
+        console.log("Data added successfully");
+        console.log(response);
+        $("#addQuestionModal").modal("hide");
+      },
+      error: function (xhr, status, error) {
+        // Handle the error
+        console.error("Error adding data:", error);
+      },
+    });
+  });
+}
---
+++ evaluation_form.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 13, 2023 at 10:03 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `evaluation_form`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `person`
+--
+
+CREATE TABLE `person` (
+  `person_id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `question_id` int(11) NOT NULL,
+  `question` varchar(255) NOT NULL,
+  `choice_a` varchar(255) NOT NULL,
+  `choice_b` varchar(255) NOT NULL,
+  `choice_c` varchar(255) NOT NULL,
+  `choice_d` varchar(255) NOT NULL,
+  `correct_answer` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `scores`
+--
+
+CREATE TABLE `scores` (
+  `person_id` int(11) NOT NULL,
+  `question_id` int(11) NOT NULL,
+  `correct_score` int(11) NOT NULL,
+  `wrong_score` int(11) NOT NULL,
+  `score` int(11) NOT NULL,
+  `time_finished` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `person`
+--
+ALTER TABLE `person`
+  ADD PRIMARY KEY (`person_id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`question_id`);
+
+--
+-- Indexes for table `scores`
+--
+ALTER TABLE `scores`
+  ADD PRIMARY KEY (`person_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `person`
+--
+ALTER TABLE `person`
+  MODIFY `person_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `scores`
+--
+ALTER TABLE `scores`
+  MODIFY `person_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
