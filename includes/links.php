c06898a8d2886f7fdbe660108140eb86aa48413a
mjasper30 <jasper.macaraeg42@gmail.com>
2023-07-15 00:05:44 +0800

    animate css link

--- includes/links.php
+++ includes/links.php
@@ -15,3 +15,5 @@
 <link rel="stylesheet" href="css/dashboard.css" />
 <!-- Icons Bootstrap -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+<!--Animate CSS-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
