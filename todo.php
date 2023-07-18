b10d3d939e1a62676104e4614f2af1cdd9bd1298
mjasper30 <jasper.macaraeg42@gmail.com>
2023-07-18 20:40:18 +0800

    just a todo list

---
+++ todo.php
@@ -0,0 +1,13 @@
+<?php
+
+/** <- use this to make bullet list
+ * Method
+ * Bullet list
+ * Another bullet
+// !Example of red Comment
+// *Example of green Comment
+// ? Should this application whould blablah blue
+// TODO: Your todo list
+// @param myParam The parameter for this method
+
+ */
