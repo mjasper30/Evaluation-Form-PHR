333d7459479094376ca6947ff8d0a9cd42f368db
mjasper30 <jasper.macaraeg@gmail.com>
2023-08-03 08:18:41 +0800

    question pdf format updated

--- admin/generate_questions_fpdf.php
+++ admin/generate_questions_fpdf.php
@@ -1,9 +1,17 @@
 <?php
 require('../fpdf/fpdf.php'); // Include the FPDF library
 
+date_default_timezone_set('Asia/Manila');
+
+
 // Function to generate the PDF
 function generatePDF($title, $name, $questions)
 {
+    // Create a DateTime object with the current date
+    $now = new DateTime();
+
+    // Format the date in a human-readable format
+    $formattedDate = $now->format('F j, Y');
     $pdf = new FPDF();
     $pdf->AddPage();
 
@@ -14,11 +22,21 @@ function generatePDF($title, $name, $questions)
     // Set font and size for title
     $pdf->SetFont('Arial', 'B', 18);
     $pdf->Cell(0, 10, $title, 0, 1, 'C');
+    $pdf->Ln(1); // Add some spacing
+
+    // Set font and size for title
+    $pdf->SetFont('Arial', 'B', 18);
+    $pdf->Cell(0, 10, '________________' , 0, 1, 'C');
     $pdf->Ln(10); // Add some spacing
 
     // Set font and size for name
     $pdf->SetFont('Arial', 'B', 12);
-    $pdf->Cell(0, 10, 'Name: ' . $name, 0, 1, 'L');
+    $pdf->Cell(0, 10, "Employer's Name: " . $name, 0, 1, 'L');
+    $pdf->Ln(1); // Add some spacing after the name
+
+    // Set font and size for name
+    $pdf->SetFont('Arial', 'B', 12);
+    $pdf->Cell(0, 10, "Date: " . $formattedDate, 0, 1, 'L');
     $pdf->Ln(10); // Add some spacing after the name
 
     // Set font and size for questions and choices
@@ -63,7 +81,7 @@ if ($result->num_rows > 0) {
 $conn->close();
 
 // Call the function to generate the PDF
-$title = "PHR Evaluation Form"; // Set the title here
+$title = "Questions"; // Set the title here
 $name = "___________________________"; // Set the name here
 generatePDF($title, $name, $questions);
 ?>
